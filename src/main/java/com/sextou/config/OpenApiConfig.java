7c28bf33962380e2a6082a05f93f078c064cc977
Fabio <fabiot92@gmail.com>
2025-03-21 16:21:45 -0300

    add swagger

---
+++ src/main/java/com/sextou/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.sextou.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Sextou API")
+                        .version("1.0")
+                        .description("API para gerenciar bares, playlists e promoções de sexta-feira"));
+    }
+}
